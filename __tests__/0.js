ef39884348ac0b33f16e06ec79f086612bb6ae8c
Adam Wong <awong@opengov.com>
2017-02-24 01:00:23 -0800

    add dockerfile

--- __tests__/0.js
+++ __tests__/0.js
@@ -1,6 +1,12 @@
+/**
+ * Represents a book.
+ * @constructor
+ * @param {string} a - The title of the book.
+ * @param {string} b - The author of the book.
+ */
 function sum(a, b) {
   return a + b;
 }
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+test('adds 1 + 2 to equal 3', function() {
+  return expect(sum(1, 2)).toBe(3);
 });
