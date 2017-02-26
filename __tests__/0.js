544bedfb2a9f88f8f9421630537ca5807c4dcb9f
adam wong <adamwong246@gmail.com>
2017-02-26 12:00:39 -0800

    failing tests

--- __tests__/0.js
+++ __tests__/0.js
@@ -5,7 +5,7 @@
  * @param {string} b - The author of the book.
  */
 function sum(a, b) {
-  return a + b;
+  return a - b;
 }
 test('adds 1 + 2 to equal 3', function() {
   return expect(sum(1, 2)).toBe(3);
